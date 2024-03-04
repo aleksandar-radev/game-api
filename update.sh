80aadc109175c1d9efc31aef1415b9a6f2f76c1b
Aleksandar Radev <alexander.radev.96@gmail.com>
2024-03-04 20:22:33 +0200

    x

--- update.sh
+++ update.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname "$0")
+ENV_DIR=~/env/app1
+
 # Navigate to the script's directory (in case script is ran from a different directory)
-cd "$(dirname "$0")"
+cd "${SCRIPT_DIR}"
 
 # Pull the latest changes from Git
 git fetch
 git reset --hard origin/main
 
 # Then navigate one directory up to where docker-compose.yml is expected to be
-cd ..
+cd ~
 
 # Add the .env file to the project's root directory
-mv ./env/app1/.env.production "$(dirname "$0")"
-mv .env.production .env
+copy "${ENV_DIR}/.env.production" "${SCRIPT_DIR}"
+mv "${SCRIPT_DIR}/.env.production" "${SCRIPT_DIR}/.env"
 
 # Rebuild the Docker image for app1
 docker-compose build app1
