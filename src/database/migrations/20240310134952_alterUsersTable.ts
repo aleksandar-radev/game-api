1265e3ab5a05d8685bbe01fe5e0ad878f48e0d70
Aleksandar Radev <alexander.radev.96@gmail.com>
2024-03-10 19:30:32 +0200

    remove test migration

--- src/database/migrations/20240310134952_alterUsersTable.ts
+++
@@ -1,9 +0,0 @@
-import type { Knex } from "knex";
-
-export async function up(knex: Knex): Promise<void> {
-  await knex.schema.alterTable("users", (t) => {
-    t.string("password").notNullable();
-  });
-}
-
-export async function down(knex: Knex): Promise<void> {}
