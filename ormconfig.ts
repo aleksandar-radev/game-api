36e35bcae05b2e677d536e4fdd3925e73a34eb7e
Aleksandar Radev <alexander.radev.96@gmail.com>
2024-07-28 20:58:21 +0300

    fix orm config

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,29 +1,32 @@
-import * as dotenv from "dotenv";
-import { DataSourceOptions } from "typeorm";
+import * as dotenv from 'dotenv';
+import { DataSourceOptions } from 'typeorm';
 dotenv.config();
 
+const isProd = process.env.NODE_ENV === 'production';
+const fileExtension = isProd ? 'js' : 'ts';
+
 export const config: DataSourceOptions = {
-  type: "postgres",
+  type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   schema: process.env.DB_SCHEMA,
-  entities: ["src/models/*.ts"],
-  migrations: ["src/database/migrations/*.ts"],
+  entities: [`src/models/*.${fileExtension}`],
+  migrations: [`src/database/migrations/*.${fileExtension}`],
   synchronize: false,
 };
 
 export const testConfig: DataSourceOptions = {
-  type: "postgres",
+  type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  schema: "test",
-  entities: ["src/models/*.ts"],
-  migrations: ["src/database/migrations/*.ts"],
+  schema: 'test',
+  entities: [`src/models/*.${fileExtension}`],
+  migrations: [`src/database/migrations/*.${fileExtension}`],
   synchronize: true,
 };
